01aa6685b46ffa3f77e665772a9129847823182b
Gustavo <gmonzong@miumg.edu.gt>
2023-05-31 23:46:55 -0600

    login mas o menos

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,33 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Button } from '@paljs/ui/Button';
 import { InputGroup } from '@paljs/ui/Input';
-import React from 'react';
 import Link from 'next/link';
-
 import Auth from 'components/Auth';
 import Layout from 'Layouts';
 
-export default function Login() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+
+  const router = useRouter();
+
+  const handleEmailChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRoleChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setRole(event.target.value);
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    localStorage.setItem('email', email);
+
+    router.push('/dashboard');
+  };
+
   return (
-    <Layout title="Login">
-      <Auth title="Login" subTitle="Hola Mundo">
-        <form>
+    <Layout title="Seguridad 101">
+      <Auth title="Ingrese sus Credenciales" subTitle="Solo Personal Autorizado">
+        <form onSubmit={handleSubmit}>
           <InputGroup fullWidth>
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
           </InputGroup>
           <InputGroup fullWidth>
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
           </InputGroup>
-          <Button status="Success" type="button" shape="SemiRound" fullWidth>
+          <Button status="Success" type="submit" shape="SemiRound" fullWidth>
             Login
           </Button>
         </form>
         <p>
-          Registro
           <Link href="/auth/register">
             <a>Register</a>
+          </Link>
+        </p>
+        <p>
+          <Link href="/dashboard">
+            <a>Home Page</a>
           </Link>
         </p>
       </Auth>
     </Layout>
   );
-}
+};
+
+export default Login;
